2b6210b704cae09d5a97687c8588cc7079b9337d
Dimitris Alexopoulos <glumfishfish@gmail.com>
2023-11-21 15:54:05 +0000

    Add ALU module

---
+++ src/alu.sv
@@ -0,0 +1,15 @@
+module alu(
+    input logic [31:0] ALUop1,
+    input logic [31:0] ALUop2,
+    input logic ALUctrl,
+    output logic [31:0] ALUout,
+    output logic EQ
+);
+
+always_comb
+    begin
+        ALUout = ALUop1 + ALUop2;
+        EQ = ALUop1 == ALUop2;
+    end 
+
+endmodule
