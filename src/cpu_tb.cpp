dc14400a353ff801363461801e5ab1d979f55dfd
Toby Browne <toby@tobybrowne.co.uk>
2023-11-23 15:30:30 +0000

    Add CPU testbench

---
+++ src/cpu_tb.cpp
@@ -0,0 +1,55 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vcpu.h"
+#include <chrono>
+#include <thread>
+
+#include "vbuddy.cpp"
+#define MAX_SIM_CYC 10000
+
+int main(int argc, char **argv, char **env){
+    int simcyc;
+    int tick;
+
+    Verilated::commandArgs(argc, argv);
+    Vcpu* top = new Vcpu;
+    
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    top->open("cpu.vcd");
+
+    //init vbuddy
+    if(vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 4: RISC-V CPU");
+
+    // set rotary button to "one-shot" mode
+    vbdSetMode(1);
+
+    //initialise simulation inputs
+    top->rst = 0;
+    top->clk = 0;
+    // run simulation for MAX_SIM_CYC cycles
+    for(simcyc = 0; simcyc < MAX_SIM_CYC; simcyc++){
+        for (tick = 0; tick < 2; tick++){
+            tfp->dump(2 * simcyc + tick);
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        // rotary encoder can be used to slow simulation
+        std::this_thread::sleep_for(std::chrono::milliseconds(10 * vbdValue()));
+
+        top->rst = vbdFlag();
+
+        // displays CPU output
+        vbdHex(2, top->a0);
+
+        if((Verilated::gotFinish()) || (vbdGetKey() == "q"))
+            exit(0);
+
+    }
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
