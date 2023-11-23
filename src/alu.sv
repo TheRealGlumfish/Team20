673f6f94700cb366375aebf4e32d510df87d71eb
Dimitris Alexopoulos <glumfishfish@gmail.com>
2023-11-23 12:10:22 +0000

    Make `ALUctrl` 3-bit

--- src/alu.sv
+++ src/alu.sv
@@ -1,7 +1,7 @@
 module alu(
     input logic [31:0] ALUop1,
     input logic [31:0] ALUop2,
-    input logic ALUctrl,
+    input logic [2:0] ALUctrl,
     output logic [31:0] ALUout,
     output logic EQ
 );
