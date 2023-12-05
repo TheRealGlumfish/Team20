2fc2c9d2a171449ae5b436ea4eb2b5171f818fb3
Toby James Browne <toby@tobybrowne.co.uk>
2023-12-05 16:55:03 +0000

    Define size of instruction memory and add memory offset arithmetic

--- rtl/src/instrmem.sv
+++ rtl/src/instrmem.sv
@@ -5,12 +5,12 @@ module instrmem(
 
 // byte addressable memory
 // define data size then NUMBER OF addresses
-logic [7:0] mem_array [512:8];
+logic [7:0] mem_array [4095:0];
 
-// a fail-safe, ensures you only read from the beginning
-// of a word.
-logic [31:0] addr = {pc[31:2], 2'b0};
+// a fail-safe, ensures you only read from the beginning of a word.
+logic[31:0] addr = {pc[31:2], 2'b0} - 'hbfc00000;
 
+// reads 4 bytes at a time, starting from the input address
 assign dout = {mem_array[addr], mem_array[addr+1], mem_array[addr+2],mem_array[addr+3]};
 
 // initialises memory with the program.
