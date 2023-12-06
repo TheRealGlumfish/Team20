7072c5a31555c46021c278bc53cc9826b2ae6bec
Toby James Browne <toby@tobybrowne.co.uk>
2023-12-06 14:40:35 +0000

    Divide 32 bit write data across 4 byte addresses

--- rtl/datamem.sv
+++ rtl/datamem.sv
@@ -11,7 +11,14 @@ logic [7:0] mem_array [131071:0];
 
 always_ff @(posedge clk)
     begin
-        if(wen) mem_array[addr] <= wdata;
+        if(wen)
+            begin
+                mem_array[addr] <= wdata[0:7];
+                mem_array[addr+1] <= wdata[8:15];
+                mem_array[addr+2] <= wdata[16:23];
+                mem_array[addr+3] <= wdata[24:31];
+            end
+
         dout <= {mem_array[addr], mem_array[addr+1], mem_array[addr+2], mem_array[addr+3]};
     end
 
