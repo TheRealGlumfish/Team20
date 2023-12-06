b8ed391982ad2360a732e8fdde4f0dde5e97e629
Dimitris Alexopoulos <glumfishfish@gmail.com>
2023-12-06 14:20:58 +0000

    Fix compilation errors in signextend unit

--- rtl/se.sv
+++ rtl/se.sv
@@ -1,7 +1,5 @@
-module se #(
-    parameter INSTRUCTION_WIDTH = 32
-) (
-    input logic [INSTRUCTION_WIDTH-1:0] Instr,
+module se(
+    input logic [31:0] Instr,
     input logic [1:0] ImmSrc,
     output logic [31:0] ImmExt
 );
@@ -9,11 +7,13 @@ module se #(
 always_comb begin
     case(ImmSrc)
     2'b00:
-        assign ImmExt = {{20{Instr[31]}}, Instr[31:20]} ;
+        ImmExt = {{20{Instr[31]}}, Instr[31:20]};
     2'b01:
-        assign ImmExt = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]} ;
+        ImmExt = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]};
     2'b10:
-        assign ImmExt = {{20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0} ;
+        ImmExt = {{20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0};
+	default:
+		ImmExt = 32'hDEAD; // error magic number
     endcase
 end
 endmodule
