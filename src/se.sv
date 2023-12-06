bfb0598fd83290dd9f46d2d54c1686f41bbc6e5e
Toby James Browne <toby@tobybrowne.co.uk>
2023-12-06 11:46:06 +0000

    Update sign extend for 2 bit immsrc

--- src/se.sv
+++ src/se.sv
@@ -1,13 +1,18 @@
 module se(
     input logic [31:0] instr,
-    input logic ImmSrc,
+    input logic [1:0] ImmSrc,
     output logic [31:0] ImmOp
 );
 
 always_comb
-    if(ImmSrc)
-        ImmOp = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-    else
-        ImmOp = {{20{instr[31]}}, instr[31:20]};
-
+    begin
+        case(ImmSrc)
+        2'b00:
+            assign ImmOp = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+        2'b01:
+            assign ImmOp = {{20{instr[31]}}, instr[31:20]};
+        2'b10:
+            assign ImmOp = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+        endcase
+    end
 endmodule
