084e9ee2e0677fadec1be0822816c87f8f22cf03
Dimitris Alexopoulos <glumfishfish@gmail.com>
2023-12-08 13:11:23 +0000

    Add support for I-Type and S-Type instructions

--- rtl/cu.sv
+++ rtl/cu.sv
@@ -27,20 +27,53 @@ begin
         7'b0110011: // R-Type instructions
         begin
             PCsrc = 0;
-            ResultSrc = 2'b00;
+            ResultSrc = 2'b00; // read from ALU
             MemWrite = 0;
             ALUsrc = 0;
-            ImmSrc = 2'b00; // Don't care
+            ImmSrc = 3'b000; // Don't care
             RegWrite = 1
             ALUctrl = {funct7[5], funct3}; 
         end
         7'b0000011: // I-Type instructions (load)
-
+        begin
+           PCsrc = 0;
+           ResultSrc = 2'b01; // read from datamem
+           MemWrite = 0;
+           ALUsrc = 1;
+           ImmSrc = 3'b000;
+           RegWrite = 1;
+           ALUctrl = 4'b0000;
+        end
         7'b0010011: // I-Type instructions (arithmetic)
-
+        begin
+            PCsrc = 0;
+            ResultSrc = 2'b00; // read from ALU
+            MemWrite = 0;
+            ALUsrc = 1;
+            ImmSrc = 3'b000;
+            RegWrite = 1
+            ALUctrl = {funct7[5], funct3}; 
+        end
         7'b0100011: // S-Type instructions
-
+        begin
+            PCsrc = 0;
+            ResultSrc= 2'b00; // Don't care
+            MemWrite = 1;
+            ALUsrc = 1;
+            ImmSrc = 3'b000;
+            RegWrite = 0
+            ALUctrl = 4'b0000;
+            // case(funct3)
+            //     3'b000: // store byte
+            //
+            //     3'b001: // store half
+            //
+            //     3'b010: // store word
+            //
+            // endcase
+        end
         7'b1100011: // B-Type instructions
+
     endcase
 end
 
