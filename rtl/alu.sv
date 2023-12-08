7c10a3821896fbd2b07d18a90ea0b97623f39aa7
Dimitris Alexopoulos <glumfishfish@gmail.com>
2023-12-08 14:13:34 +0000

    Add support for S-Type and B-Type instructions

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,14 +1,14 @@
 module alu(
     input logic [31:0] ALUop1,
     input logic [31:0] ALUop2,
-    input logic [2:0] ALUctrl,
+    input logic [3:0] ALUctrl,
     output logic [31:0] ALUout,
     output logic Zero
 );
 
 always_comb
 begin
-    case(ALUctrl) // TODO: Add XOR somehow
+    case(ALUctrl)
         4'b0000: // add  
             ALUout = ALUop1 + ALUop2;
         4'b1001: // subtract
@@ -18,7 +18,7 @@ begin
         4'b0010: // less than
             ALUout = {31'b0, ALUop1 < ALUop2};
         4'b0011: // less than unsigned
-            ALUout = {31'b0, ALUop1 < ALUop2};
+            ALUout = {31'b0, ALUop1 < ALUop2}; // TODO: FIX
         4'b0100: // bitwise XOR
             ALUout = ALUop1 ^ ALUop2;
         4'b0101: // logical shift right
--- rtl/cu.sv
+++ rtl/cu.sv
@@ -8,8 +8,7 @@ module cu(
     output logic ALUsrc,
     output logic [1:0]  ResultSrc,
     output logic [2:0] ImmSrc,
-    output logic [3:0] ALUctrl,
-    output logic Jump
+    output logic [3:0] ALUctrl
 );
 
 logic [6:0] op;
@@ -19,7 +18,7 @@ logic [2:0] funct3;
 assign op = instr[6:0];
 assign funct7 = instr[31:25];
 assign funct3 = instr[14:12];
-
+assign JALR = 0;
 
 always_comb
 begin
@@ -31,7 +30,7 @@ begin
             MemWrite = 0;
             ALUsrc = 0;
             ImmSrc = 3'b000; // Don't care
-            RegWrite = 1
+            RegWrite = 1;
             ALUctrl = {funct7[5], funct3}; 
         end
         7'b0000011: // I-Type instructions (load)
@@ -51,7 +50,7 @@ begin
             MemWrite = 0;
             ALUsrc = 1;
             ImmSrc = 3'b000;
-            RegWrite = 1
+            RegWrite = 1;
             ALUctrl = {funct7[5], funct3}; 
         end
         7'b0100011: // S-Type instructions
@@ -61,7 +60,7 @@ begin
             MemWrite = 1;
             ALUsrc = 1;
             ImmSrc = 3'b000;
-            RegWrite = 0
+            RegWrite = 0;
             ALUctrl = 4'b0000;
             // case(funct3)
             //     3'b000: // store byte
@@ -73,7 +72,45 @@ begin
             // endcase
         end
         7'b1100011: // B-Type instructions
-
+        begin
+            ResultSrc = 2'b00;
+            MemWrite = 1;
+            ALUsrc = 1;
+            ImmSrc = 3'b00; // Potentially matters
+            RegWrite = 0;
+            case(funct3)
+                3'b000: // beq
+                begin
+                    PCsrc = Zero;
+                    ALUctrl = 4'b1001;
+                end
+                3'b001: // bne
+                begin
+                    PCsrc = !Zero;
+                    ALUctrl = 4'b1001;
+                end
+                3'b100: // blt
+                begin
+                    PCsrc = !Zero;
+                    ALUctrl = 4'b0011;
+                end
+                3'b101: // bge
+                begin
+                    PCsrc = Zero;
+                    ALUctrl = 4'b0010;
+                end
+                3'b110: // bltu
+                begin
+                    PCsrc = !Zero;
+                    ALUctrl = 4'b0011;
+                end
+                3'b111: // bgeu
+                begin
+                    PCsrc = Zero;
+                    ALUctrl = 4'b0011;
+                end
+            endcase
+        end
     endcase
 end
 
