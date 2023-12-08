73e5bba2b392c0068fd0eac87bcaad4cd75215a9
Dimitris Alexopoulos <glumfishfish@gmail.com>
2023-12-08 15:07:10 +0000

    Add support for U-Type and J-Type instructions

--- rtl/cu.sv
+++ rtl/cu.sv
@@ -18,13 +18,13 @@ logic [2:0] funct3;
 assign op = instr[6:0];
 assign funct7 = instr[31:25];
 assign funct3 = instr[14:12];
-assign JALR = 0;
+assign JALR = op == 7'b1100111;
 
 always_comb
 begin
     case(op)
         7'b0110011: // R-Type instructions
-        begin
+        begin // TODO: Check/fix unsigned variants
             PCsrc = 0;
             ResultSrc = 2'b00; // read from ALU
             MemWrite = 0;
@@ -34,7 +34,7 @@ begin
             ALUctrl = {funct7[5], funct3}; 
         end
         7'b0000011: // I-Type instructions (load)
-        begin
+        begin // TODO: Check/fix unsigned variants
            PCsrc = 0;
            ResultSrc = 2'b01; // read from datamem
            MemWrite = 0;
@@ -42,6 +42,18 @@ begin
            ImmSrc = 3'b000;
            RegWrite = 1;
            ALUctrl = 4'b0000;
+           // case(funct3)
+           //      3'b000: // load byte
+           //
+           //      3'b001: // load half
+           //
+           //      3'b010: // load word
+           //
+           //      3'b100: // load byte unsigned
+           //
+           //      3'b101: // load half unsigned
+           //
+           // endcase
         end
         7'b0010011: // I-Type instructions (arithmetic)
         begin
@@ -52,6 +64,16 @@ begin
             ImmSrc = 3'b000;
             RegWrite = 1;
             ALUctrl = {funct7[5], funct3}; 
+        end
+        7'b1100111: // I-Type instructions (jalr)
+        begin
+            PCsrc = 1;
+            ResultSrc = 2'b10;
+            MemWrite = 0;
+            ALUsrc = 1;
+            ImmSrc = 3'b000;
+            RegWrite = 1;
+            ALUctrl = 4'b1001;
         end
         7'b0100011: // S-Type instructions
         begin
@@ -110,6 +132,30 @@ begin
                     ALUctrl = 4'b0011;
                 end
             endcase
+        end
+        // 7'b0010111: // U-Type instructions (auipc)
+        // begin
+        // 
+        // end
+        7'b0110111: // U-Type instructions (lui)
+        begin
+            PCsrc = 0;
+            ResultSrc = 2'b11;
+            MemWrite = 0;
+            ALUsrc = 0; // Don't care
+            ImmSrc = 3'b100;
+            RegWrite = 1;
+            ALUctrl = 4'b0000; // Don't care
+        end
+        7'b1101111: // J-Type instructions (jal)
+        begin
+           PCsrc = 1;
+           ResultSrc = 2'b10;
+           MemWrite = 0;
+           ALUctrl = 4'b1001;
+           ALUsrc = 1;
+           ImmSrc = 3'b011;
+           RegWrite = 1;
         end
     endcase
 end
