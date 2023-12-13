d30eb8a39165390b7a7b47cc16e8b8403e6f8547
AEljaa <adamtantalum@gmail.com>
2023-12-13 19:59:14 +0000

    Need to add  cu signals to say wheter we have a read mem instruction (to read from cache) or write to cache - more work to be done

--- Cache/cache_tb.cpp
+++ Cache/cache_tb.cpp
@@ -1,6 +1,7 @@
 #include "Vcache.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include <iostream>
 /*
     input clk,
     input rst,
@@ -28,8 +29,9 @@ int main(int argc,char **argv, char **env){
 
     top->addr = 0xFFFFFFF4; //SET 001 TAG 1111
     top->datain=0x73737373;
-
+    top->wen = 1;
     for (i=0; i<40; i++){
+        std::cout << "i: " << i << std::endl;
 
         for (clk =0; clk<2; clk++){
             tfp->dump (2*i +clk);
@@ -37,9 +39,20 @@ int main(int argc,char **argv, char **env){
             top->clk = !top->clk;
 
         }
-        if (i>10){
-            top->addr = 0xEFFFFFF4; //SET 101 TAG EFF....
+        if (i==10){
+            top->addr = 0xEFFFFFE4; //SET 101 TAG EFF....
             top->datain=0xAAAAAAAA;
+        }
+        else if (i==11){
+            top->addr = 0xEFFFFFE0; //SET 101 TAG EFF....
+            top->datain=0xAAABBAAA;
+        }
+        else if (i==18){
+            top->addr = 0xEDFFFFE0; //SET 101 TAG EFF....
+            top->datain=0xAAABDEEA;
+        }
+        if(i>18){
+            top->wen = 0;
         }
 
         if(Verilated::gotFinish()) exit(0);
