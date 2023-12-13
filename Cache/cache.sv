fd080dbe7cd1ecb7d438d4a5ba69b161eadd7e4a
Adam El Jaafari <70715586+AEljaa@users.noreply.github.com>
2023-12-13 16:48:18 +0000

    Update cache.sv
    Direct map cache should now be fully parasitised, we can decide how many cache blocks we want to have. I also fixed an issue where cache outputs (if hit) were not asynchronous. Only writing is synchronous.

--- Cache/cache.sv
+++ Cache/cache.sv
@@ -1,5 +1,5 @@
 module cache#(
-    parameter setlength = 3  //a set length of 3 means 8 chache blocks
+    parameter setlength = 3  //a set length of 3 means 8 cache blocks
 )
 (
     input clk,
@@ -12,24 +12,23 @@ module cache#(
 );
 
 
-logic [59:0] cacheblocks [2**setlength-1:0] ; // our set is 3 bits and so at most 8 cache blocks 
+logic [62-setlength:0] cacheblocks [2**setlength-1:0] ; // our set is 3 bits and so at most 8 cache blocks 
 //input 
-logic [26:0] tag_i = addr[31:setlength+2];
-logic [2:0]  set_i = addr[setlength+1:2]; 
+logic [29-setlength:0] tag_i = addr[31:setlength+2];
+logic [setlength-1:0]  set_i = addr[setlength+1:2]; 
 //cache
-logic v_c = cacheblocks[set_i][59];
-logic [26:0] tag_c = cacheblocks[set_i][58:32];
+logic v_c = cacheblocks[set_i][62-setlength];
+logic [29-setlength:0] tag_c = cacheblocks[set_i][61-setlength:32];
 //hit
 assign Hit = (v_c && tag_c==tag_i) ;
 
+always_comb begin
+    if (Hit) cache_out = cacheblocks[set_i][31:0];
+end
+
 always_ff @(negedge clk) begin
-    if (Hit)begin
-        cache_out <= cacheblocks[set_i][31:0];
-    end
-    else begin
-        cacheblocks[set_i][31:0] <= datain ; //assign the data of the address into the cache if there was not a hit
-        cacheblocks[set_i][58:32] <= tag_i; //replace the old tag with now the new tag of what we are replacing
-        cacheblocks[set_i][59] <=1 ; //cache v bit is now valid (1)
-    end
-    end
+    cacheblocks[set_i][31:0] <= datain ; //assign the data of the address into the cache if there was not a hit
+    cacheblocks[set_i][61-setlength:32] <= tag_i; //replace the old tag with now the new tag of what we are replacing
+    cacheblocks[set_i][62-setlength] <=1 ; //cache v bit is now valid (1)
+end
 endmodule
