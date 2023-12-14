98350f82bd02266163da26e254afa55de889e784
AEljaa <adamtantalum@gmail.com>
2023-12-14 18:00:28 +0000

    Direct map cache had an error that the tagof the cache and the tag of the address where one clcok cycle delayed,this was fixed by just changign always comb to always latch - cache should be good now since same gaussian works

--- rtl/cache.sv
+++ rtl/cache.sv
@@ -8,20 +8,22 @@ module cache #(
     input logic [31:0] addr, //[Tag,set,byte_offset]
     input logic [31:0] ramdata,
     input logic [31:0] wdatain, 
-    output logic [31:0] cache_out,
-    output logic Hit
+    output logic [31:0] cache_out
 );
-//TODO : ADD cu signals for chace wheter we have read from mem or write from meme
-logic [62-setlength:0] cacheblocks [2**setlength-1:0]; // our set is 3 bits and so at most 8 cache blocks
+
+logic [59:0] cacheblocks [2**setlength-1:0] ; // our set is 3 bits and so at most 8 cache blocks
 //input 
-logic [29-setlength:0] tag_i = addr[31:setlength+2];
-logic [setlength-1:0] set_i = addr[setlength+1:2]; 
+logic [26:0] tag_i = addr[31:setlength+2];
+logic [2:0]  set_i = addr[setlength+1:2]; 
 //cache
-logic v_c = cacheblocks[set_i][62-setlength];
-logic [29-setlength:0] tag_c = cacheblocks[set_i][61-setlength:32];
+logic v_c ;
+logic [26:0] tag_c ;
+
+assign v_c = cacheblocks[set_i][59];
+assign tag_c = cacheblocks[set_i][58:32];
 
 //logic that outputs the value of cache if HIT or value of Ram data if miss
-always_comb begin
+always_latch begin
     if(v_c && tag_c == tag_i) begin
         case(DataWidth)
             3'b000:begin//LW
@@ -58,25 +60,25 @@ always_ff@(posedge clk)begin
             case(DataWidth)
                 3'b000:begin //sw
                     cacheblocks[set_i][31:0] <= wdatain; // assign word of the data of the address into the cache 
-                    cacheblocks[set_i][61-setlength:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
-                    cacheblocks[set_i][62-setlength] <= 1; // cache v bit is now valid (1)
+                    cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
+                    cacheblocks[set_i][59] <= 1; // cache v bit is now valid (1)
                 end
                 3'b001:begin//shw
                 cacheblocks[set_i][15:0] <= wdatain[15:0]; // assign half of data of the address into the cache 
-                cacheblocks[set_i][61-setlength:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
+                cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
                 cacheblocks[set_i][62-setlength] <= 1; // cache v bit is now valid (1)
                 end
                 3'b010:begin //sb
                 cacheblocks[set_i][7:0] <= wdatain[7:0]; // assign byte of data of the address into the cache if there was not a hit
-                cacheblocks[set_i][61-setlength:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
-                cacheblocks[set_i][62-setlength] <= 1; // cache v bit is now valid (1)
+                cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
+                cacheblocks[set_i][59] <= 1; // cache v bit is now valid (1)
                 end
             endcase
         end
         if(!(v_c && tag_c == tag_i))begin //if we dont have a hit then get the value at the adress from ram 
             cacheblocks[set_i][31:0] <= ramdata; // assign the data of the address into the cache if there was not a hit
-            cacheblocks[set_i][61-setlength:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
-            cacheblocks[set_i][62-setlength] <= 1; // cache v bit is now valid (1)
+            cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
+            cacheblocks[set_i][59] <= 1; // cache v bit is now valid (1)
 
         end
 end
