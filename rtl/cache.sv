f7afcfac2f508b602833e22613c01c554dcddd61
AEljaa <adamtantalum@gmail.com>
2023-12-14 18:59:51 +0000

    Fully paramtiased cache again, and i changed so that we write to cache if miss of the neg edge, this prevented me from having issues where the hit rate was 1 (since it wasnt synchronous- i assume writing to cache should be sysnchronous) and the negedge synced up to the tags of the input and cache and its the only way i could get cache working

--- rtl/cache.sv
+++ rtl/cache.sv
@@ -10,21 +10,22 @@ module cache #(
     input logic [31:0] wdatain, 
     output logic [31:0] cache_out
 );
-
-logic [59:0] cacheblocks [2**setlength-1:0] ; // our set is 3 bits and so at most 8 cache blocks
+logic [62-setlength:0] cacheblocks [2**setlength-1:0]; // our set is 3 bits and so at most 8 cache blocks
 //input 
-logic [26:0] tag_i = addr[31:setlength+2];
-logic [2:0]  set_i = addr[setlength+1:2]; 
+logic [29-setlength:0] tag_i = addr[31:setlength+2];
+logic [setlength-1:0] set_i = addr[setlength+1:2]; 
 //cache
 logic v_c ;
-logic [26:0] tag_c ;
+logic [29-setlength:0] tag_c ;
+logic Hit;
 
-assign v_c = cacheblocks[set_i][59];
-assign tag_c = cacheblocks[set_i][58:32];
+assign v_c = cacheblocks[set_i][62-setlength];
+assign tag_c = cacheblocks[set_i][61-setlength:32];
+assign Hit = (v_c && tag_c == tag_i);
 
 //logic that outputs the value of cache if HIT or value of Ram data if miss
-always_latch begin
-    if(v_c && tag_c == tag_i) begin
+always_comb begin
+    if(Hit) begin
         case(DataWidth)
             3'b000:begin//LW
                 cache_out = cacheblocks[set_i][31:0];
@@ -48,39 +49,42 @@ always_latch begin
         endcase
 
     end
-    else begin
+    else begin//miss
         cache_out = ramdata;
+
     end
 end
 
 //only alter cache if we have a instruction that uses memory/cache
 always_ff@(posedge clk)begin
-    if(cacheEn) //if we have a cache/ mem type instruction
-        if(wen)begin //if we want to write to cache / memory then write regardless (doesn't need a hit)
-            case(DataWidth)
-                3'b000:begin //sw
-                    cacheblocks[set_i][31:0] <= wdatain; // assign word of the data of the address into the cache 
-                    cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
-                    cacheblocks[set_i][59] <= 1; // cache v bit is now valid (1)
-                end
-                3'b001:begin//shw
-                cacheblocks[set_i][15:0] <= wdatain[15:0]; // assign half of data of the address into the cache 
-                cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
+    if(cacheEn) //if we have a cache/  mem type instruction 
+        if(wen)begin
+        case(DataWidth)
+            3'b000:begin //sw
+                cacheblocks[set_i][31:0] <= wdatain; // assign word of the data of the address into the cache 
+                cacheblocks[set_i][61-setlength:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
                 cacheblocks[set_i][62-setlength] <= 1; // cache v bit is now valid (1)
-                end
-                3'b010:begin //sb
+            end
+            3'b001:begin//shw
+                cacheblocks[set_i][15:0] <= wdatain[15:0]; // assign half of data of the address into the cache 
+                cacheblocks[set_i][61-setlength:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
+                cacheblocks[set_i][62-setlength] <= 1; // cache v bit is now valid (1)
+            end
+            3'b010:begin //sb
                 cacheblocks[set_i][7:0] <= wdatain[7:0]; // assign byte of data of the address into the cache if there was not a hit
-                cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
-                cacheblocks[set_i][59] <= 1; // cache v bit is now valid (1)
-                end
-            endcase
-        end
-        if(!(v_c && tag_c == tag_i))begin //if we dont have a hit then get the value at the adress from ram 
-            cacheblocks[set_i][31:0] <= ramdata; // assign the data of the address into the cache if there was not a hit
-            cacheblocks[set_i][58:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
-            cacheblocks[set_i][59] <= 1; // cache v bit is now valid (1)
-
-        end
+                cacheblocks[set_i][61-setlength:32] <= tag_i; // replace the old tag with now the new tag of what we are replacing
+                cacheblocks[set_i][62-setlength] <= 1; // cache v bit is now valid (1)
+            end
+        endcase
+    end
 end
-
+//if we miss write in negdge (to avoid the issues of the delayed by one clk signal tags)
+always_ff@(negedge clk)begin    
+    if(!Hit)begin
+        //we missed and so we have to store the value of ram into cache
+        cacheblocks[set_i][31:0] = ramdata; // assign the ram data of the address into the cache if there was not a hit
+        cacheblocks[set_i][61-setlength:32] = tag_i; // replace the old tag with now the new tag of what we are replacing
+        cacheblocks[set_i][62-setlength] = 1; // cache v bit is now valid (1)
+    end
+end
 endmodule
